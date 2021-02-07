264923e094998659fbcc17292954b0e1f454d2a5
Waqas Bilal <waqasbilal02@gmail.com>
2021-02-07 16:04:15 +0500

    Adding some info

--- app.py
+++ app.py
@@ -187,6 +187,102 @@ def predict_skin():
 @app.route("/kidney", methods=['GET', 'POST'])
 def kidney():
     return render_template('kidney.html')
+@app.route("/kidney_predict", methods=['GET', 'POST'])
+def kidney_predict():
+    model = pickle.load(open(r'Model\kidney_disease_ab1_model.sav', 'rb'))
+    print("@@ Kidney Model Loaded")
+    if request.method=='POST':
+        try:
+            age=float(request.form['age'])
+            bp=float(request.form['bp'])
+            sg=float(request.form['sg'])
+            al=float(request.form['al'])
+            su=float(request.form['su'])
+            bgr=float(request.form['bgr'])
+            bu=float(request.form['bu'])
+            sc=float(request.form['sc'])
+            sod=float(request.form['sod'])
+            pot=float(request.form['pot'])
+            hemo=float(request.form['hemo'])
+            pcv=float(request.form['pcv'])
+            wc=float(request.form['wc'])
+            rc=float(request.form['rc'])
+        except Exception as e:
+            print("@@",e)
+            return render_template('kidney.html',prediction_text="Some unknown error occured please input the values in number or contact the develpor if it still occurs")
+        pc_=request.form['pc']
+        if pc_=='normal':
+            pc_normal=1
+            pc_nan=0
+        elif pc_=='nan':
+            pc_normal=0
+            pc_nan=1
+        else:
+            pc_normal=0
+            pc_nan=0
+        pcc_=request.form['pcc']
+        if pcc_=='present':
+            pcc_present=1
+        else:
+            pcc_present=0
+        ba_=request.form['ba']
+        if ba_=='present':
+            ba_present=1
+        else:
+            ba_present=0    
+        htn_=request.form['htn']
+        if htn_=='yes':
+            htn_yes=1
+        else:
+            htn_yes=0
+        dm_=request.form['dm']
+        if dm_=='yes':
+            dm_no=0
+            dm_yes=1
+        else:
+            dm_no=0
+            dm_yes=0
+
+        cad_=request.form['cad']
+        if cad_=='yes':
+            cad_yes=1
+        else:
+            cad_yes=0
+        appet_=request.form['appet']
+        if appet_=='poor':
+            appet_poor=1
+        else:
+            appet_poor=0
+        pe_=request.form['pe']
+        if pe_=='yes':
+            pe_yes=1
+        else:
+            pe_yes=0
+        ane_=request.form['ane']
+        if ane_=='yes':
+            ane_yes=1
+        else:
+            ane_yes=0
+        rbc_=request.form['rbc']
+        if rbc_=='normal':
+            rbc_normal=1
+            rbc_nan=0
+        elif rbc_=='nan':
+            rbc_normal=0
+            rbc_nan=1
+        else:
+            rbc_normal=0
+            rbc_nan=0
+        X=pd.DataFrame([[age, bp, sg, al, su, bgr, bu, sc, sod, pot, hemo,pcv, wc, rc, rbc_normal, rbc_nan, pc_normal, pc_nan,pcc_present, ba_present, htn_yes, dm_no, dm_yes, cad_yes,appet_poor, pe_yes, ane_yes]],columns=['age', 'bp', 'sg', 'al', 'su', 'bgr', 'bu', 'sc', 'sod', 'pot', 'hemo','pcv', 'wc', 'rc', 'rbc_normal', 'rbc_nan', 'pc_normal', 'pc_nan','pcc_present', 'ba_present', 'htn_yes', 'dm_no', 'dm_yes', 'cad_yes','appet_poor', 'pe_yes', 'ane_yes'])
+
+        print(X)
+        X_col=X.columns[[ True,  True, False,  True, False,  True,  True,  True,  True,True,  True,  True,True,  True,  True,  True,  True, False,False, False,  True, False,  True,  True, False, False,True]]
+        output=model.predict(X[X_col])
+        print(output)
+        if output==0:
+            return render_template('kidney.html',prediction_text="Result: \nPrediction Result: Don't worry You don't have any Kidney disease!")
+        elif output==1:
+            return render_template('kidney.html',prediction_text="Result: \nWe found something wrong with your kidney, please consult with the doctor")
 
 
 
